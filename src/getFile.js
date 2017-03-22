59aa75bd31295b9307063266e441e2bea3410bf3
tiwu <theimaginationwasunbroken@gmail.com>
2017-03-22 22:40:44 +0900

    #7 lint and formatter

--- src/getFile.js
+++ src/getFile.js
@@ -1,28 +1,29 @@
 // git install
 require('lambda-git')({
-    updateEnv: typeof updateEnv === 'undefined' ? true : updateEnv
+  updateEnv: typeof updateEnv === 'undefined' ? true : updateEnv,
 });
 
 // git setting
 const config = require('config');
+
 const id = config.id;
 const pass = config.pass;
 const repogitory = config.repogitory;
-const repogitoryUrl = 'https://' + id + ':' + pass + '@' + repogitory;
+const repogitoryUrl = `https://${id}:${pass}@${repogitory}`;
 const localDir = '/tmp/awp/';
 
 const fs = require('fs');
 const simpleGit = require('simple-git');
 
 module.exports = (filePath) => {
-    const date = new Date();
-    const cloneDir = localDir + date.getTime() + '/';
+  const date = new Date();
+  const cloneDir = `${localDir}${date.getTime()}/`;
 
-    return new Promise((resolve, reject) => {
-        simpleGit().clone(repogitoryUrl, cloneDir, '', () => {
-            fs.readFile(cloneDir + filePath, 'utf-8', (err, data) => {
-                resolve(data);
-            });
-        });
+  return new Promise((resolve) => {
+    simpleGit().clone(repogitoryUrl, cloneDir, '', () => {
+      fs.readFile(cloneDir + filePath, 'utf-8', (err, data) => {
+        resolve(data);
+      });
     });
+  });
 };
