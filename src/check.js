7ad49d4ea069211b513faec03cf21fad248ee22c
s_sasaki <s_sasaki@active.co.jp>
2017-04-14 11:49:36 +0900

    ・変数宣言をvarからletにし、ブロックレベルでの変数スコープに変更しました。 ・関数宣言をアロー演算子にし、省略記法としました。 ・IF文の分岐を「===」にし、値・型完全一致である条件に変更しました。

--- src/check.js
+++ src/check.js
@@ -1,4 +1,4 @@
-exports.handler = function (event, context) {
+exports.handler = (event, context) => {
     console.log('チェック処理を開始します。');
     const msg = JSON.parse(event.Records[0].Sns.Message);
     console.log(msg);
@@ -6,11 +6,11 @@ exports.handler = function (event, context) {
     const branchName = msg.ref;
     console.log('msg.refの値は' + branchName);
     
-    var filePath = '';
+    let filePath = '';
     console.log('msg.head_commit.addedの値は' + msg.head_commit.added);
     console.log('msg.head_commit.addedの要素数は' + msg.head_commit.added.length);
     
-    if ( branchName == 'refs/heads/master' && msg.head_commit.added.length > 0) {
+    if ( branchName === 'refs/heads/master' && msg.head_commit.added.length > 0) {
         console.log('マスターに対するPUSHのため、ファイル名取得処理を開始します。');
         filePath = msg.head_commit.added;
         
