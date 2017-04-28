1522a8b12821df4474d27d8eed6af102f57cc593
s_sasaki <s_sasaki@active.co.jp>
2017-04-28 17:31:11 +0900

    エンコードをShift-JisからUTF8に変更しました。

--- src/check.js
+++ src/check.js
@@ -1,22 +1,22 @@
 module.exports = (event, context) => {
-    console.log('�`�F�b�N�������J�n���܂��B');
+    console.log('チェック処理を開始します。');
     const msg = JSON.parse(event.Records[0].Sns.Message);
     console.log(msg);
     
     const branchName = msg.ref;
-    console.log('msg.ref�̒l��' + branchName);
+    console.log('msg.refの値は' + branchName);
     
     let filePath = '';
-    console.log('msg.head_commit.added�̒l��' + msg.head_commit.added);
-    console.log('msg.head_commit.added�̗v�f����' + msg.head_commit.added.length);
+    console.log('msg.head_commit.addedの値は' + msg.head_commit.added);
+    console.log('msg.head_commit.addedの要素数は' + msg.head_commit.added.length);
     
     if ( branchName === 'refs/heads/master' && msg.head_commit.added.length > 0) {
-        console.log('�}�X�^�[�ɑ΂���PUSH�̂��߁A�t�@�C�����擾�������J�n���܂��B');
+        console.log('マスターに対するPUSHのため、ファイル名取得処理を開始します。');
         filePath = msg.head_commit.added;
-        console.log('getFile.js�Ƀt�@�C���p�X[' + filePath + ']�𑗂�܂��B');
+        console.log('getFile.jsにファイルパス[' + filePath + ']を送ります。');
         
     } else {
-        console.log('�u�����`���F[' + branchName + ']�ɑ΂���PUSH�܂��͕ύX�ɑ΂���}�[�W�̂��߁A�t�@�C�������M���s�킸�������I�����܂��B');
+        console.log('ブランチ名：[' + branchName + ']に対するPUSHまたは変更に対するマージのため、ファイル名送信を行わず処理を終了します。');
     }
 
     return filePath;
