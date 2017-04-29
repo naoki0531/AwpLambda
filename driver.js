9b1f670510e996e06b10255a9115bf205cb25269
丸山直樹 <andvari100@gmail.com>
2017-04-29 21:58:45 +0900

    make local setting, lint checkjs

--- driver.js
+++ driver.js
@@ -1,11 +1,23 @@
-var event = {
-    'filePath': '2017/0321.md',
+let event = {
+  Records: [{Sns: {Message: {}}}]
 };
-var context = {
-    done: function(err,message){
-        return;
-    }
+let context = {
+  done: function (err, message) {
+    return;
+  }
 };
 updateEnv = false;
-var lambd = require("./index.js");
-lambd.handler(event, context);
+const lambd = require('./index.js');
+const fs = require('fs');
+
+// set test json data name
+let jsonName = './event/masterPush.json';
+if (process.argv[2] !== undefined) {
+  jsonName = './event/' + process.argv[2];
+}
+
+fs.readFile(jsonName, 'utf8', (err, data) => {
+  event.Records[0].Sns.Message = data;
+  lambd.handler(event, context);
+})
+
